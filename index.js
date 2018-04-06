b9fbe0d8009a9e852bac16761f24f328474f614c
Anna Veselova <anna.veselova411@gmail.com>
2018-04-05 22:45:46 -0500

    db querying works

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
 const pg = require('pg');
 const url = require('url')
 
-const params = url.parse(process.env.DATABASE_URL);
-if(params.auth == null) {
-	console.log("running locally")
-	//running locally
-	var pool = new pg.Pool()
-} else {
-	console.log("using database url")
-	//heroku run with DATABASE_URL
+var config = null;
+if(process.env.DATABASE_URL) {
+	const params = url.parse(process.env.DATABASE_URL);
 	const auth = params.auth.split(':');
-
-	const config = {
+	config = {
 	  user: auth[0],
 	  password: auth[1],
 	  host: params.hostname,
 	  port: params.port,
 	  database: params.pathname.split('/')[1],
 	  ssl: true
-	};
-
-	var pool = new pg.Pool(config);
+	}
 }
+var pool = new pg.Pool(config);
 
+//var query = "INSERT INTO urls(url, name, title) VALUES('https://www.theatlantic.com/science/archive/2018/04/the-scientific-paper-is-obsolete/556676/','Anna','The Scientific Paper Is Obsolete');"
 
-pool.query('INSERT INTO urls(url, name, title) VALUES(\'https://www.theatlantic.com/politics/archive/2018/04/why-big-business-isnt-defending-amazon-against-trump/557264/\',\'Anna\',\'Why Big Business Isn\'t Defending Amazon Against Trump\');', (err, res) => {
+var url_val = "https://www.theatlantic.com/politics/archive/2018/04/why-big-business-isnt-defending-amazon-against-trump/557264/"
+var title_val = "Why Big Business Isn''t Defending Amazon Against Trump"
+var name_val = "Anna"
+
+var query = "INSERT INTO urls(url, name, title) VALUES(\'" 
+	+ url_val + "\',\'" + name_val + "\',\'" + title_val + "\');";
+
+pool.query(query, (err, res) => {
   console.log(err,res);
   pool.end();
 });
-
-/*
-pool.query('UPDATE users SET firstName=\'Brian\', lastName=\'Pynkley\' WHERE id=2;', (err, res) => {
-  console.log(err,res);
-  pool.end();
-});
-
-pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  pool.end();
-});
-*/
