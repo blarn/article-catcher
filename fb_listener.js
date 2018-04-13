3008a36ec4ffafe4a1da8d30cf96d1d96e719475
Anna Veselova <anna.veselova411@gmail.com>
2018-04-13 04:10:40 -0500

    no more hardcoding shit

--- fb_listener.js
+++ fb_listener.js
@@ -5,18 +5,6 @@ require(__dirname + '/db.js')();
 
 var urlRE= new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\\s]{2,}|www\.[a-zA-Z0-9]\.[^\\s]{2,})",'g');
 
-var id_nums = {
-	Brad:  100001935746541,
-	Anna:  100000798878012
-};
-var id_names = {
-	100001935746541: "Brad",
-	100000798878012: "Anna"
-}
-
-var tID = id_nums[process.env.THREAD_ID || "Anna"];
-var data = [];
-
 login({appState: JSON.parse(fs.readFileSync(__dirname + '/appstate.json', 'utf8'))}, (err, api) => {
 	if(err) return console.error(err);
 	
@@ -24,17 +12,36 @@ login({appState: JSON.parse(fs.readFileSync(__dirname + '/appstate.json', 'utf8'
 		selfListen: true
 	});
 
+	var id_names = {};
+
+	var tID = process.env.THREAD_ID || api.getCurrentUserID();
+
 	api.listen((err, message) => {
 		if(err) return console.error(err);
 		if(message.threadID == tID) {
-			console.log("time: "+message.timestamp+" sender: "+message.senderID+" message: "+message.body)
+
+			//gets names in advance (even for non-url messages)
+			var id = message.senderID
+			var name = "Unknown"
+			if(id in id_names) {
+				var name = id_names[id]
+			} else {
+				api.getUserInfo([id], (err, ret) => {
+					if(err) return console.error(err);
+					if(ret.hasOwnProperty(id) && ret[id].firstName != null) {
+						name = ret[id].firstName
+						id_names[id] = name;
+					}
+				});
+			}
+			console.log(name+": "+message.body)
+
+			//URLs:
 			var match = (message.body).match(urlRE);
 			if(match) {
 				var l = match.length;
 				for (var i = 0; i < l; i++) {
 					var url = match[i];
-					var name = id_names[message.senderID]
-					if(name == null) name = "unknown";
 					urlMetadata(url).then(
 						function (metadata) {
 							if(metadata["og:type"]=="article") {
@@ -47,8 +54,15 @@ login({appState: JSON.parse(fs.readFileSync(__dirname + '/appstate.json', 'utf8'
 						function (err) {
 							console.log(err)
 							add_url(url, name, null, message.timestamp)
-						});
+						}
+					);
 				}
+			}
+
+			//Stickers:
+			if(message.attachments.length==1 && message.attachments[0].type=='sticker') {
+				console.log("Sticker: "+message.attachments[0].caption)
+				//add_sticker(message.attachments[0].ID, message.attachments[0].url, 1)
 			}
 		}
 	});
