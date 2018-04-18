a754470b5342cabe68c966c1fc81c5a69ad7ed7f
Anna Veselova <anna.veselova411@gmail.com>
2018-04-17 20:48:43 -0500

    revert to normal fb_cache_login

--- fb_cache_login.js
+++ fb_cache_login.js
@@ -3,7 +3,7 @@ const login = require("facebook-chat-api");
 
 var credentials = {email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD};
 
-login({appState: JSON.parse(fs.readFileSync(__dirname + '/appstate.json', 'utf8'))}, (err, api) => {
+login(credentials, (err, api) => {
     if(err) return console.error(err);
     fs.writeFileSync(__dirname + '/appstate.json', JSON.stringify(api.getAppState()));
 });
